173edd3418af6769da46e03504875129374a19bf
Brainskull24 <96540981+Brainskull24@users.noreply.github.com>
2023-01-19 19:39:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -21,32 +21,15 @@ job.addEventListener("click", () => {
   selfcont.classList.remove("self-emp");
   jobcont.classList.add("job");
 });
-burger = document.querySelector('.burger');
-navbar = document.querySelector('.navbar');
-navList = document.querySelector('.nav-list');
-rightnav = document.querySelector('.right-nav-btn');
 
-burger.addEventListener('click' , ()=> {
-  navList.classList.toggle('v-class');
-  rightnav.classList.toggle('v-class');
-  navbar.classList.toggle('h-nav');
-});
 
-const swiper = new Swiper('.swiper', {
-      autoplay : {
-       delay: 3000,
-       displayOnInterction: false,
-      },
-      
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-});
-console.log("hello")
-// const sigup = document.getElementsBy
+// burger = document.querySelector('.burger');
+// navbar = document.querySelector('.navbar');
+// navList = document.querySelector('.nav-list');
+// rightnav = document.querySelector('.right-nav-btn');
+
+// burger.addEventListener('click' , ()=> {
+//   navList.classList.toggle('v-class');
+//   rightnav.classList.toggle('v-class');
+//   navbar.classList.toggle('h-nav');
+// });
